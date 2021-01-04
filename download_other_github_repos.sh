e83aa117b07cc1a3e81e1110d45993905ceba2ff
richelbilderbeek <richel@richelbilderbeek.nl>
2021-01-04 20:47:42 +0100

    Go GHA

---
+++ download_other_github_repos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ..
+
+git clone https://github.com/richelbilderbeek/DotMatrix
+git clone https://github.com/richelbilderbeek/plane
+git clone https://github.com/richelbilderbeek/RibiClasses
+git clone https://github.com/richelbilderbeek/RibiLibraries
+git clone https://github.com/richelbilderbeek/TicTacToe
+git clone https://github.com/richelbilderbeek/relearn
+
